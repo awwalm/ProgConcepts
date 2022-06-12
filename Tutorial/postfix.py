7a3da1ef15ce66ea95198c73da1ed2b43602eae7
Awwal M <67423428+awwalm@users.noreply.github.com>
2022-06-12 12:59:17 +0400

    Added postfix calculator draft program

---
+++ Tutorial/postfix.py
@@ -0,0 +1,10 @@
+'''Postfix Bivariate Calculator'''
+
+
+def add(x, z, ):
+    
+    sum1= (x)+(z)
+    print(int(sum1))
+
+add(int(input('Enter an operation in postfix: ')),int(input('Enter another: ')))
+

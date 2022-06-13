6200dcb82b5dd7d3eba6eceb98a32db969bcd9ce
Awwal M <67423428+awwalm@users.noreply.github.com>
2022-06-13 20:13:07 +0400

    More basic Python features

---
+++ Python Practice/poem.py
@@ -0,0 +1,8 @@
+def DaysOfTheMonth():
+    print('Thirty Days Hath September,')
+    print('April, June And November.')
+    print('All The Rest Are Thirty-one')
+    print('Except February Alone.')
+DaysOfTheMonth()
+DaysOfTheMonth()
+DaysOfTheMonth()
